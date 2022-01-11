95abbb92ef14c34a14a78614119d26f3d770645b
灬凯凯凯凯利、 <75210220+hukl1993@users.noreply.github.com>
2022-01-12 01:14:47 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
         System.out.println("灬凯凯凯凯利、来了！");
+        System.out.println("开启新篇章吧！");
     }
 }
